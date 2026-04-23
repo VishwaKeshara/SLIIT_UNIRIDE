a886453ec20994fd0907cf1ff1da9af8dff861fa
chamalshalekamge <chamalshalekamge78@gmail.com>
2026-04-23 23:38:32 +0530

    Update mobile number validation

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -8,7 +8,7 @@ exports.createBooking = async (req, res) => {
     const { passengerName, mobileNumber, route, travelStartDate, travelEndDate } = req.body;
 
     if (!passengerName)    return res.status(400).json({ message: "Passenger name is required!" });
-    if (!mobileNumber)     return res.status(400).json({ message: "Mobile number is required" });
+    if (!mobileNumber)     return res.status(400).json({ message: "Mobile number is required!" });
     if (!route)            return res.status(400).json({ message: "Route is required" });
     if (!travelStartDate)  return res.status(400).json({ message: "Travel start date is required" });
     if (!travelEndDate)    return res.status(400).json({ message: "Travel end date is required" });
